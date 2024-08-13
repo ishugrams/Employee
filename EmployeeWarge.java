a37ca0bd89d8954194619a6661c83daa9e8406fb
Isha Shinde <ishashinde2021@gmail.com>
2024-08-13 14:02:31 +0530

    Added: If emplyeeis present or not

--- EmployeeWarge.java
+++ EmployeeWarge.java
@@ -5,5 +5,17 @@ class EmployeeWarge
     public static void main(String arg[])
     {
         System.out.println("Welcome to EmployeeWarge..");
+
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+
+        if(attendance == 1)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
